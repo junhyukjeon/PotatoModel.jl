b08c001b74bf343606085b44c3f1a3d13dd5c65d
Jun Hyuk Jeon <jnhykjeon@gmail.com>
2022-10-11 13:45:05 -0700

    Test

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,15 @@ using PotatoModel
 using Documenter
 
 makedocs(
-         sitename = "PotatoModel.jl",
-         modules  = [PotatoModel],
-         pages=[
-                "Home" => "index.md"
-               ])
-               
+    sitename = "PotatoModel.jl",
+    modules  = [PotatoModel],
+    pages = ["Home" => "index.md"]
+    )
+    format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    )
+
 deploydocs(;
-    repo="github.com/junhyukjeon/PotatoModel.jl",
+    repo = "github.com/junhyukjeon/PotatoModel.jl",
+    devbranch = "master"
 )
